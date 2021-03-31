96804ccdf5af95272cfd7ac01f1fb4bc587bc5cb
olaconecta <olaconecta@gmail.com>
2021-03-31 14:35:42 -0700

    colunas

--- eduardolevi/js/app.js
+++ eduardolevi/js/app.js
@@ -4,23 +4,43 @@ $(function() {
   event.preventDefault();
   });
 });		
+var btHome = document.getElementById("btHome");
+var btCareer = document.getElementById("btCareer");
+var btWorks = document.getElementById("btWorks");
+var btMe = document.getElementById("btMe");
 
 // scrolls
 function scrollHome() {
     var elmnt = document.getElementById("home");
-    elmnt.scrollIntoView();
+    elmnt.scrollIntoView();    
+    btHome.classList.add('active');
+    btCareer.classList.remove('active');
+    btWorks.classList.remove('active');
+    btMe.classList.remove('active');
   } 
-function scroll1() {
+  function scroll1() {
     var elmnt = document.getElementById("content1");
     elmnt.scrollIntoView();
+    btCareer.classList.add('active');    
+    btHome.classList.remove('active');
+    btWorks.classList.remove('active');
+    btMe.classList.remove('active');
   } 
-function scroll2() {
+  function scroll2() {
     var elmnt = document.getElementById("content2");
     elmnt.scrollIntoView();
+    btWorks.classList.add('active');    
+    btHome.classList.remove('active');
+    btCareer.classList.remove('active');
+    btMe.classList.remove('active');
   } 
-function scroll3() {
+  function scroll3() {
     var elmnt = document.getElementById("content3");
     elmnt.scrollIntoView();
+    btMe.classList.add('active');
+    btHome.classList.remove('active');
+    btCareer.classList.remove('active');
+    btWorks.classList.remove('active');
   } 
 
 //   function scrollWin() {
